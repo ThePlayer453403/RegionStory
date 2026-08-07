9253329770c3c14ff1fdd7cbea78373a7ab6e188
ThePlayer453403 <theplayer453403@163.com>
2026-08-08 03:46:09 +0800

    删除无用代码

--- src/main/java/com/regionstory/mixin/MouseMixin.java
+++ src/main/java/com/regionstory/mixin/MouseMixin.java
@@ -1,10 +1,7 @@
 package com.regionstory.mixin;
 
-import com.regionstory.client.CameraTransitionController;
 import com.regionstory.client.RegionStoryClient;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
-import net.minecraft.client.input.MouseInput;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
